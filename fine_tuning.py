c8b0504b8a2f47c29d65a6663e55109b9a36cb3b
Jaykang <mind2749@naver.com>
2023-08-04 14:51:50 +0900

    Refactor: refactoring all codes

--- fine_tuning.py
+++ fine_tuning.py
@@ -7,7 +7,6 @@ from PIL import Image
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
-
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -16,305 +15,160 @@ import torchvision
 from torchvision import models, transforms
 
 
-class ImageTransform():
-    """
-    이미지 전처리 클래스. 훈련시, 검증시의 동작이 다르다.
-    이미지 크기를 리사이즈하고, 색상을 표준화한다.
-    훈련시에는 RandomResizedCrop과 RandomHorizontalFlip으로 데이터를 확장한다.
-
-    Attributes
-    ----------
-    resize : int
-        리사이즈 대상 이미지 크기.
-    mean : (R, G, B)
-        각 색상 채널의 평균 값.
-    std : (R, G, B)
-        각 색상 채널의 표준 편차.
-    """
-
+class ImageTransform:
     def __init__(self, resize, mean, std):
         self.data_transform = {
-            'train': transforms.Compose([
-                transforms.RandomResizedCrop(
-                    resize, scale=(0.5, 1.0)),  # 데이터 확장
-                transforms.RandomHorizontalFlip(),  # 데이터 확장
-                transforms.ToTensor(),  # 텐서로 변환
-                transforms.Normalize(mean, std)  # 표준화
-            ]),
-            'val': transforms.Compose([
-                transforms.Resize(resize),  # 리사이즈
-                transforms.CenterCrop(resize),  # 이미지 중앙을 resize × resize로 자른다
-                transforms.ToTensor(),  # 텐서로 변환
-                transforms.Normalize(mean, std)  # 표준화
-            ])
+            "train": transforms.Compose(
+                [
+                    transforms.RandomResizedCrop(resize, scale=(0.5, 1.0)),
+                    transforms.RandomHorizontalFlip(),
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean, std),
+                ]
+            ),
+            "val": transforms.Compose(
+                [
+                    transforms.Resize(resize),
+                    transforms.CenterCrop(resize),
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean, std),
+                ]
+            ),
         }
 
-    def __call__(self, img, phase='train'):
-        """
-        Parameters
-        ----------
-        phase : 'train' or 'val'
-            전처리 모드를 지정.
-        """
+    def __call__(self, img, phase="train"):
         return self.data_transform[phase](img)
 
-# image_file_path = './korean/0.jpg'
-# img = Image.open(image_file_path)   # [높이][폭][색RGB]
-
-# 2. 원본 이미지 표시
-# plt.imshow(img)
-# plt.show()
-
-# 3. 이미지 전처리, 처리된 이미지 표시
-size = 224
-mean = (0.485, 0.456, 0.406)
-std = (0.229, 0.224, 0.225)
-
-transform = ImageTransform(size, mean, std)
-# img_transformed = transform(img, phase="train")  # torch.Size([3, 224, 224])
-
-# (색상, 높이, 너비)를 (높이, 너비, 색상)으로 변환하고 0-1로 값을 제한해 표시
-# img_transformed = img_transformed.numpy().transpose((1, 2, 0))
-# img_transformed = np.clip(img_transformed, 0, 1)
-# plt.imshow(img_transformed)
-# plt.show()
 
 def make_datapath_list(phase="train"):
-    """
-    데이터의 경로를 저장한 리스트를 작성한다.
-
-    Parameters
-    ----------
-    phase : 'train' or 'val'
-        훈련 데이터 또는 검증 데이터를 지정
-
-    Returns
-    -------
-    path_list : list
-        데이터 경로를 저장한 리스트
-    """
-    if phase == 'train':
-        target_path = osp.join('./**/*.jpg')
+    if phase == "train":
+        target_path = osp.join("./**/*.jpg")
     else:
-        target_path =osp.join('./**/**/*.jpg')
+        target_path = osp.join("./**/**/*.jpg")
 
     print(target_path)
-    path_list = []  # 여기에 저장한다
-
-    # glob을 이용하여 하위 디렉토리의 파일 경로를 가져온다
-    for path in glob.glob(target_path):
-        path_list.append(path)
+    path_list = [path for path in glob.glob(target_path)]
 
     return path_list
 
 
-# 실행
-train_list = make_datapath_list(phase="train")
-val_list = make_datapath_list(phase='val')
-# train_list
-
-
-class myDataset(data.Dataset):
-    """
-    PyTorch의 Dataset 클래스를 상속한다.
-
-    Attributes
-    ----------
-    file_list : 리스트
-        이미지 경로를 저장한 리스트
-    transform : object
-        전처리 클래스의 인스턴스
-    phase : 'train' or 'test'
-        학습인지 훈련인지를 설정한다.
-    """
-
-    def __init__(self, file_list, transform=None, phase='train'):
-        self.file_list = file_list  # 파일 경로 리스트
-        self.transform = transform  # 전처리 클래스의 인스턴스
-        self.phase = phase  # train or val 지정
+class CustomDataset(data.Dataset):
+    def __init__(self, file_list, transform=None, phase="train"):
+        self.file_list = file_list
+        self.transform = transform
+        self.phase = phase
 
     def __len__(self):
-        '''이미지 개수를 반환'''
         return len(self.file_list)
 
     def __getitem__(self, index):
-        '''
-        전처리한 이미지의 Tensor 형식의 데이터와 라벨을 취득
-        '''
-
-        # index번째의 이미지 로드
         img_path = self.file_list[index]
-        img = Image.open(img_path)  # [높이][너비][색RGB]
+        img = Image.open(img_path)
 
-        # 이미지 전처리를 실시
-        img_transformed = self.transform(
-            img, self.phase)  # torch.Size([3, 224, 224])
+        img_transformed = self.transform(img, self.phase)
 
-        label = img_path.split('/')[1]
-
-        if label == 'val':
-            label = img_path.split('/')[2]
-
-        # 라벨을 숫자로 변경
-        if label == "italian":
-            label = 0
-        elif label == "korean":
-            label = 1
+        label = osp.basename(osp.dirname(img_path))
+        label = 0 if label == "italian" else 1
 
         return img_transformed, label
 
 
-# 실행
-train_dataset = myDataset(
-    file_list=train_list, transform=ImageTransform(size, mean, std), phase='train')
-
-val_dataset = myDataset(
-    file_list=val_list, transform=ImageTransform(size, mean, std), phase='val')
-
-# 동작 확인
-index = 32
-print(val_dataset.__getitem__(index)[0].size())
-print(val_dataset.__getitem__(index)[1])
+def create_data_loader(dataset, batch_size, shuffle):
+    return torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
 
 
-batch_size = 32
-
-# DataLoader 작성
-train_dataloader = torch.utils.data.DataLoader(
-    train_dataset, batch_size=batch_size, shuffle=True)
-
-val_dataloader = torch.utils.data.DataLoader(
-    val_dataset, batch_size=batch_size, shuffle=False)
-
-# 사전형 변수에 정리
-dataloaders_dict = {"train": train_dataloader, "val": val_dataloader}
+def initialize_model(num_classes):
+    use_pretrained = True
+    net = models.vgg16(pretrained=use_pretrained)
+    net.classifier[6] = nn.Linear(in_features=4096, out_features=num_classes)
+    return net
 
 
-# 학습된 VGG-16 모델을 로드
-# VGG-16 모델의 인스턴스 생성
-use_pretrained = True  # 학습된 파라미터를 사용
-net = models.vgg16(pretrained=use_pretrained)
-
-# VGG16의 마지막 출력층의 출력 유닛을 개미와 벌의 2개로 바꾼다
-net.classifier[6] = nn.Linear(in_features=4096, out_features=2)
-
-# 훈련 모드로 설정
-net.train()
-print('네트워크 설정 완료: 학습된 가중치를 읽어들여 훈련 모드로 설정했습니다.')
-
-# 손실 함수 설정
-criterion = nn.CrossEntropyLoss()
-
-# 파인 튜닝으로 학습할 파라미터를 params_to_update 변수의 1~3에 저장한다
-
-params_to_update_1 = []
-params_to_update_2 = []
-params_to_update_3 = []
-
-# 학습시킬 층의 파라미터명을 지정
-update_param_names_1 = ["features"]
-update_param_names_2 = ["classifier.0.weight",
-                        "classifier.0.bias", "classifier.3.weight", "classifier.3.bias"]
-update_param_names_3 = ["classifier.6.weight", "classifier.6.bias"]
-
-# 파라미터를 각각 리스트에 저장
-for name, param in net.named_parameters():
-    if update_param_names_1[0] in name:
-        param.requires_grad = True
-        params_to_update_1.append(param)
-        print("params_to_update_1에 저장: ", name)
-
-    elif name in update_param_names_2:
-        param.requires_grad = True
-        params_to_update_2.append(param)
-        print("params_to_update_2에 저장: ", name)
-
-    elif name in update_param_names_3:
-        param.requires_grad = True
-        params_to_update_3.append(param)
-        print("params_to_update_3에 저장: ", name)
-
-    else:
-        param.requires_grad = False
-        print("경사 계산없음. 학습하지 않음: ", name)
+def set_parameter_requires_grad(model, update_param_names, requires_grad=True):
+    for name, param in model.named_parameters():
+        if any(name.startswith(update_name) for update_name in update_param_names):
+            param.requires_grad = requires_grad
 
 
-# 최적화 기법 설정
-optimizer = optim.SGD([
-    {'params': params_to_update_1, 'lr': 1e-4},
-    {'params': params_to_update_2, 'lr': 5e-4},
-    {'params': params_to_update_3, 'lr': 1e-3}
-], momentum=0.9)
-
-
-# 모델을 학습시키는 함수를 작성
-def train_model(net, dataloaders_dict, criterion, optimizer, num_epochs):
-
-    # 초기 설정
-    # GPU가 사용 가능한지 확인
+def train(net, dataloaders, criterion, optimizer, num_epochs):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    print("사용 장치: ", device)
-
-    # 네트워크를 GPU로
     net.to(device)
 
-    # 네트워크가 어느 정도 고정되면, 고속화시킨다
-    torch.backends.cudnn.benchmark = True
-
-    # epoch 루프
     for epoch in range(num_epochs):
-        print('Epoch {}/{}'.format(epoch+1, num_epochs))
-        print('-------------')
+        print(f"Epoch {epoch + 1}/{num_epochs}")
+        print("-------------")
 
-        # epoch별 훈련 및 검증 루프
-        for phase in ['train', 'val']:
-            if phase == 'train':
-                net.train()  # 모델을 훈련 모드로
+        for phase in ["train", "val"]:
+            is_training = phase == "train"
+            if is_training:
+                net.train()
             else:
-                net.eval()   # 모델을 검증 모드로
+                net.eval()
 
-            epoch_loss = 0.0  # epoch 손실의 합
-            epoch_corrects = 0  # epoch 정답수
+            epoch_loss = 0.0
+            epoch_corrects = 0
 
-            # 미학습시의 검증 성능을 확인하기 위해 epoch=0의 훈련은 생략
-            if (epoch == 0) and (phase == 'train'):
+            if epoch == 0 and is_training:
                 continue
 
-            # 데이터 로더에서 미니 배치를 꺼내 루프
-            for inputs, labels in tqdm(dataloaders_dict[phase]):
-
-                # GPU가 사용 가능하면 GPU에 데이터 보내기
+            for inputs, labels in tqdm(dataloaders[phase]):
                 inputs = inputs.to(device)
                 labels = labels.to(device)
 
-                # optimizer를 초기화
                 optimizer.zero_grad()
 
-                # 순전파(forward) 계산
-                with torch.set_grad_enabled(phase == 'train'):
+                with torch.set_grad_enabled(is_training):
                     outputs = net(inputs)
-                    loss = criterion(outputs, labels)  # 손실 계산
-                    _, preds = torch.max(outputs, 1)  # 라벨 예측
+                    loss = criterion(outputs, labels)
+                    _, preds = torch.max(outputs, 1)
 
-                    # 훈련시에는 오차 역전파법
-                    if phase == 'train':
+                    if is_training:
                         loss.backward()
                         optimizer.step()
 
-                    # 결과 계산
-                    epoch_loss += loss.item() * inputs.size(0)  # loss의 합계를 갱신
-                    # 정답 수의 합계를 갱신
+                    epoch_loss += loss.item() * inputs.size(0)
                     epoch_corrects += torch.sum(preds == labels.data)
 
-            # epoch별 loss와 정답률을 표시
-            epoch_loss = epoch_loss / len(dataloaders_dict[phase].dataset)
-            epoch_acc = epoch_corrects.double(
-            ) / len(dataloaders_dict[phase].dataset)
+            epoch_loss = epoch_loss / len(dataloaders[phase].dataset)
+            epoch_acc = epoch_corrects.double() / len(dataloaders[phase].dataset)
 
-            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
-                phase, epoch_loss, epoch_acc))
+            print(f"{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}")
 
-# 학습 및 검증을 실행
-num_epochs=5
-train_model(net, dataloaders_dict, criterion, optimizer, num_epochs=num_epochs)
 
+def main():
+    size = 224
+    mean = (0.485, 0.456, 0.406)
+    std = (0.229, 0.224, 0.225)
+    batch_size = 32
+    num_epochs = 5
+
+    transform = ImageTransform(size, mean, std)
+    train_list = make_datapath_list(phase="train")
+    val_list = make_datapath_list(phase="val")
+
+    train_dataset = CustomDataset(file_list=train_list, transform=transform, phase="train")
+    val_dataset = CustomDataset(file_list=val_list, transform=transform, phase="val")
+
+    train_dataloader = create_data_loader(train_dataset, batch_size, shuffle=True)
+    val_dataloader = create_data_loader(val_dataset, batch_size, shuffle=False)
+
+    dataloaders_dict = {"train": train_dataloader, "val": val_dataloader}
+
+    num_classes = 2
+    net = initialize_model(num_classes)
+
+    set_parameter_requires_grad(net, ["features"])
+    set_parameter_requires_grad(
+        net, ["classifier.0.weight", "classifier.0.bias", "classifier.3.weight", "classifier.3.bias"]
+    )
+    set_parameter_requires_grad(net, ["classifier.6.weight", "classifier.6.bias"], requires_grad=True)
+
+    optimizer = optim.SGD(net.parameters(), lr=1e-4, momentum=0.9)
+
+    criterion = nn.CrossEntropyLoss()
+
+    train(net, dataloaders_dict, criterion, optimizer, num_epochs)
+
+
+if __name__ == "__main__":
+    main()
--- image_crawling.py
+++ image_crawling.py
@@ -2,38 +2,30 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import urllib.request
-
 import os
 import time
-
 import argparse
 
-parser = argparse.ArgumentParser(description='Image Crawling')
-
-parser.add_argument('--search_word', type=str, help = 'Enter searching word', dest='search_word')
-parser.add_argument('--number', type=int, help = 'Enter what you want image number', dest='num')
-parser.add_argument('--dir', type=str, help = 'Enter save image file directory', default='./')
+parser = argparse.ArgumentParser(description="Image Crawling")
+parser.add_argument("--search_word", type=str, help="Enter searching word", dest="search_word")
+parser.add_argument("--number", type=int, help="Enter what you want image number", dest="num")
+parser.add_argument("--dir", type=str, help="Enter save image file directory", default="./")
 args = parser.parse_args()
 
+
 def create_folder(dir):
-    try:
-        if not os.path.exists(dir):
-            os.makedirs(dir)
-    except OSError:
-        print('Error: Creating directory. ' + dir)
+    os.makedirs(dir, exist_ok=True)
 
-# search_word = '한식'
-create_folder('./' + args.dir)
 
-driver = webdriver.Chrome('./chromedriver')
+create_folder("./" + args.dir)
 
-print(args.search_word, '검색')
+driver = webdriver.Chrome("./chromedriver")
+print(args.search_word, "검색")
 driver.get("https://www.google.co.kr/imghp?hl=ko&tab=wi&authuser=0&ogbl")
 
 word = driver.find_element_by_xpath('//*[@id="sbtc"]/div/div[2]/input')
 word.send_keys(args.search_word)
-driver.find_element_by_xpath('//*[@id="sbtc"]/button').click()
-
+word.send_keys(Keys.RETURN)
 
 elem = driver.find_element_by_tag_name("body")
 for i in range(60):
@@ -41,36 +33,33 @@ for i in range(60):
     time.sleep(0.1)
 
 try:
-    driver.find_element_by_xpath('//*[@id="islmp"]/div/div/div/div[1]/div[2]/div[2]/input').click()
+    show_more = driver.find_element_by_xpath('//*[@id="islmp"]/div/div/div/div[1]/div[2]/div[2]/input')
+    show_more.click()
     for i in range(60):
         elem.send_keys(Keys.PAGE_DOWN)
         time.sleep(0.1)
-except:
+except selenium.common.exceptions.NoSuchElementException:
     pass
 
-links=[]
+links = []
 images = driver.find_elements_by_css_selector("img.rg_i.Q4LuWd")
 for image in images:
-    if image.get_attribute('src')!=None:
+    if image.get_attribute("src") is not None:
         if len(links) < args.num:
-            links.append(image.get_attribute('src'))
+            links.append(image.get_attribute("src"))
         else:
             break
 
-print(args.search_word+' 찾은 이미지 개수:',len(links))
+print(args.search_word + " 찾은 이미지 개수:", len(links))
 time.sleep(2)
 
-
-for k,i in enumerate(links):
-    url = i
+for k, url in enumerate(links):
     start = time.time()
     try:
-        urllib.request.urlretrieve(url, "./"+args.dir+"/"+str(k)+".jpg")
-        print(str(k+1)+'th'+' '+args.search_word+' 다운로드 중....... Download time : '+str(time.time() - start)[:5]+' 초')
-        # print(args.search_word+' ---다운로드 완료---')
-        driver.close()
-
-    except:
-        pass
+        urllib.request.urlretrieve(url, f"./{args.dir}/{k}.jpg")
+        print(f"{k+1}th {args.search_word} 다운로드 중....... Download time : {time.time() - start:.2f} 초")
+    except (urllib.error.HTTPError, urllib.error.URLError) as e:
+        print(f"Error downloading {url}: {e}")
     end = time.time()
-    print(str(end - start)[:5] + '초')
+    print(f"{end - start:.2f}초")
+driver.close()
